3640f29a2411244ec9c8f7b72429f844404a91c7
aniketsonkerr <aniketsonkerr@gmail.com>
2025-02-04 20:54:27 +0530

    connect to mongodb

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(`${process.env.MONGODB_URI}`);
+
+const db = mongoose.connection;
+
+db.on("open", () => {
+  console.log("Database connection succesfull");
+});
+
+db.on("error", () => {
+  console.log("Database connection failed");
+});
